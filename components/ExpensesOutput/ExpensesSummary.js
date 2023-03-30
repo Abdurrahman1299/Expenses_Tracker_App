e7b20b572117bb8ad638a1298abc069dd4491892
Abdurrahman1299 <abdurrahmanms09@gmail.com>
2023-03-30 20:04:58 +0200

    first-commit

---
+++ components/ExpensesOutput/ExpensesSummary.js
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+export default function ExpensesSummary() {
+  return (
+    <View>
+      <Text>last 7 days</Text>
+      <Text>$132.55</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
