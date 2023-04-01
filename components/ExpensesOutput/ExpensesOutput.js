38caffcf6dae76980e4931e10083fecd2206730d
Abdurrahman1299 <abdurrahmanms09@gmail.com>
2023-04-01 02:38:43 +0200

    apr-1

--- components/ExpensesOutput/ExpensesOutput.js
+++ components/ExpensesOutput/ExpensesOutput.js
@@ -3,11 +3,38 @@ import React from "react";
 import ExpensesSummary from "./ExpensesSummary";
 import ExpensesList from "./ExpensesList";
 
-export default function ExpensesOutput({ expenses }) {
+const DUMMY_EXPENSES = [
+  {
+    id: "e1",
+    description: "Lorem ipsum dolor sit 1",
+    amount: 34.43,
+    date: new Date("2021-12-12"),
+  },
+  {
+    id: "e2",
+    description: "Lorem ipsum dolor sit 2",
+    amount: 34.43,
+    date: new Date("2021-10-2"),
+  },
+  {
+    id: "e3",
+    description: "Lorem ipsum dolor sit 3",
+    amount: 34.43,
+    date: new Date("2021-11-5"),
+  },
+  {
+    id: "e4",
+    description: "Lorem ipsum dolor sit 4",
+    amount: 34.43,
+    date: new Date("2021-11-5"),
+  },
+];
+
+export default function ExpensesOutput({ expenses, expensesPeriod }) {
   return (
     <View>
-      <ExpensesSummary />
-      <ExpensesList />
+      <ExpensesSummary periodName={expensesPeriod} expenses={DUMMY_EXPENSES} />
+      <ExpensesList periodName={expensesPeriod} expenses={DUMMY_EXPENSES} />
     </View>
   );
 }
--- screens/RecentExpenses.js
+++ screens/RecentExpenses.js
@@ -1,10 +1,11 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
+import ExpensesOutput from "../components/ExpensesOutput/ExpensesOutput";
 
 export default function RecentExpenses() {
   return (
     <View>
-      <Text>RecentExpenses</Text>
+      <ExpensesOutput expensesPeriod={"last 7 days"} />
     </View>
   );
 }
