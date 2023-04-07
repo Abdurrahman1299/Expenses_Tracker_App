e9d24b432f82a4a3fbfcd9d17ea2577043d98716
Abdurrahman1299 <abdurrahmanms09@gmail.com>
2023-04-07 20:21:43 +0200

    apr-7

--- components/Manage Expense/Input.js
+++ components/Manage Expense/Input.js
@@ -2,20 +2,22 @@ import { StyleSheet, Text, TextInput, View } from "react-native";
 
 import { GlobalStyles } from "../../constants/styles";
 
-export default function Input({ label, textInputConfig }) {
-  let inputStyles = [styles.input];
+function Input({ label, style, textInputConfig }) {
+  const inputStyles = [styles.input];
 
   if (textInputConfig && textInputConfig.multiline) {
     inputStyles.push(styles.inputMultiline);
   }
 
   return (
-    <View style={styles.inputContainer}>
+    <View style={[styles.inputContainer, style]}>
       <Text style={styles.label}>{label}</Text>
       <TextInput style={inputStyles} {...textInputConfig} />
     </View>
   );
 }
+
+export default Input;
 
 const styles = StyleSheet.create({
   inputContainer: {
