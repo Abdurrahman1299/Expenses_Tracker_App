c37da9e9279b434f01a9351cdc5a74d1bca6d822
Abdurrahman1299 <abdurrahmanms09@gmail.com>
2023-04-09 14:44:16 +0200

    apr-9

--- screens/ManageExpense.js
+++ screens/ManageExpense.js
@@ -1,58 +1,54 @@
-import { StyleSheet, View } from "react-native";
-import React, { useContext, useLayoutEffect } from "react";
+import { useContext, useLayoutEffect } from "react";
+import { StyleSheet, TextInput, View } from "react-native";
 
-// components
+import ExpenseForm from "../components/ManageExpense/ExpenseForm";
+import Button from "../components/UI/Button";
 import IconButton from "../components/UI/IconButton";
 import { GlobalStyles } from "../constants/styles";
-import Button from "../components/UI/Button";
 import { ExpensesContext } from "../store/expenses-context";
-import ExpenseForm from "../components/Manage Expense/ExpenseForm";
 
-// END IMPORTS //
-export default function ManageExpense({ navigation, route }) {
-  const expenseCtx = useContext(ExpensesContext);
+function ManageExpense({ route, navigation }) {
+  const expensesCtx = useContext(ExpensesContext);
 
   const editedExpenseId = route.params?.expenseId;
   const isEditing = !!editedExpenseId;
 
+  const selectedExpense = expensesCtx.expenses.find(
+    (expense) => expense.id === editedExpenseId
+  );
+
   useLayoutEffect(() => {
     navigation.setOptions({
-      title: isEditing ? "Edit Expense" : "Add Espense",
+      title: isEditing ? "Edit Expense" : "Add Expense",
     });
   }, [navigation, isEditing]);
 
   function deleteExpenseHandler() {
-    expenseCtx.deleteExpense(editedExpenseId);
+    expensesCtx.deleteExpense(editedExpenseId);
     navigation.goBack();
   }
+
   function cancelHandler() {
     navigation.goBack();
   }
-  function confirmHandler() {
-    isEditing
-      ? expenseCtx.updateExpense(editedExpenseId, {
-          description: "hello",
-          amount: 6,
-          date: new Date("2023-04-04"),
-        })
-      : expenseCtx.addExpense({
-          description: "hello",
-          amount: 6,
-          date: new Date("2023-04-04"),
-        });
+
+  function confirmHandler(expenseData) {
+    if (isEditing) {
+      expensesCtx.updateExpense(editedExpenseId, expenseData);
+    } else {
+      expensesCtx.addExpense(expenseData);
+    }
     navigation.goBack();
   }
+
   return (
     <View style={styles.container}>
-      <ExpenseForm />
-      <View style={styles.buttons}>
-        <Button style={styles.button} mode="flat" onPress={cancelHandler}>
-          Cancel
-        </Button>
-        <Button style={styles.button} onPress={confirmHandler}>
-          {isEditing ? "Update" : "Add"}
-        </Button>
-      </View>
+      <ExpenseForm
+        submitButtonLabel={isEditing ? "Update" : "Add"}
+        onSubmit={confirmHandler}
+        onCancel={cancelHandler}
+        defaultValues={selectedExpense}
+      />
       {isEditing && (
         <View style={styles.deleteContainer}>
           <IconButton
@@ -67,20 +63,13 @@ export default function ManageExpense({ navigation, route }) {
   );
 }
 
+export default ManageExpense;
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 24,
     backgroundColor: GlobalStyles.colors.primary800,
-  },
-  buttons: {
-    flexDirection: "row",
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  button: {
-    minWidth: 120,
-    marginHorizontal: 8,
   },
   deleteContainer: {
     marginTop: 16,
